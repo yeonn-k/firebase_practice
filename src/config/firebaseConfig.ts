88902e8e5e747727520baf7006c43a92496591a5
yeon <yeonnnnn.k@gmail.com>
2024-10-01 00:18:49 +0900

    ✨add firebase/initialSetup

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -12,3 +13,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export { app };
+export const auth = getAuth(app);
